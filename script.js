bdc29a47495d09bf5782a10295c7b537d63cd2a5
Abraham Serena <102245246+aserenaa@users.noreply.github.com>
2024-02-04 02:06:37 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-// disable double click zoom on mobile
-document.addEventListener('touchstart', function preventZoom(e) {
-  if (e.touches.length > 1) {
-    e.preventDefault();
-  }
-}, { passive: false });
-
-
 document.getElementById('noBtn').addEventListener('click', function() {
   const yesButton = document.getElementById('yesBtn');
   let scale = parseFloat(yesButton.style.transform.replace('scale(', '').replace(')', '')) || 1;
